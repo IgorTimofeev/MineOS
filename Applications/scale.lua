e82d59b34d26d1d9b7c46fb0147d8015b9585ef2
Igor Timofeev <eliteclubsessions@gmail.com>
2015-08-04 22:38:26 +0300

    Create scale.lua

---
+++ Applications/scale.lua
@@ -0,0 +1,17 @@
+local ecs = require("ECSAPI")
+local gpu = require("component").gpu
+
+local arg = {...}
+if arg[1] == "get" or arg[1] == "show" or arg[1] == "print" or arg[1] == "write" then
+  local max1, max2 = gpu.maxResolution()
+  local cur1, cur2 = gpu.getResolution()
+  local scale = cur1/max1*100
+  print(" ")
+  print("Максимальное поддерживаемое разрешение: " .. max1 .. "x".. max2)
+  print("Текущее разрешение: " .. cur1.."x"..cur2)
+  print(" ")
+  print("Масштаб: "..scale.."%")
+  print(" ")
+else
+  ecs.setScale(tonumber(arg[1]) or 1)
+end
