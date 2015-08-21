f7fd3f19912c8067199f9fb2765435730ac405ad
Igor Timofeev <eliteclubsessions@gmail.com>
2015-08-21 17:10:27 +0300

    Update scale.lua

--- Applications/scale.lua
+++ Applications/scale.lua
@@ -2,7 +2,7 @@ local ecs = require("ECSAPI")
 local gpu = require("component").gpu
 
 local arg = {...}
-if arg[1] == "get" or arg[1] == "show" or arg[1] == "print" or arg[1] == "write" then
+if arg[1] == "get" or arg[1] == "show" or arg[1] == "print" or arg[1] == "write" or arg[1] == "info" or arg[1] == "help" then
   local max1, max2 = gpu.maxResolution()
   local cur1, cur2 = gpu.getResolution()
   local scale = cur1/max1*100
