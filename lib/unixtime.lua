aae2c69923edaef3257f2ba05e8491381d07b8f5
Igor Timofeev <eliteclubsessions@gmail.com>
2015-08-24 18:48:30 +0300

    Update unixtime.lua

--- lib/unixtime.lua
+++ lib/unixtime.lua
@@ -2,23 +2,14 @@ local unixtime = {}
 
 ----------------------------------------------------------------------------------------
 
-function unixtime.convert(unix, timezone)
+function unixtime.convert(unix)
     
     --Проверить аргументы
     checkArg(1, unix, "number")
     
-    --Часовой пояс
-    timezone = timezone or 3
-
-    --Коорекция времени
-    local t_correction = timezone * 3600 
-
-    --
-    local lastmod = tonumber(string.sub(tostring(unix), 1, -4)) + t_correction
-    -- Вариант 4, все в куче, как мы привыкли, в правильном формате
+    --Вариант 4, все в куче, как мы привыкли, в правильном формате
     --local dt = os.date('%d.%m.%Y %H:%M:%S', lastmod)
-    local dt = os.date('%d.%m.%Y', lastmod)
-    return dt
+    return os.date('%d.%m.%Y', unix)
 end
 
 ----------------------------------------------------------------------------------------
