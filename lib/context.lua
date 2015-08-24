acc0852cd605a31202c9f220b193d2384bfe2e46
Igor Timofeev <eliteclubsessions@gmail.com>
2015-08-24 12:50:01 +0300

    Update context.lua

--- lib/context.lua
+++ lib/context.lua
@@ -23,6 +23,8 @@ function context.menu(x, y, ...)
 	local data = {...}
 	local sData = #data
 
+	obj = {}
+
 	--Получаем размер экрана
 	local xSize, ySize = gpu.getResolution()
 
@@ -92,7 +94,7 @@ function context.menu(x, y, ...)
 	for key, val in pairs(obj["Elements"]) do
 		if ecs.clickedAtArea(e[3], e[4], obj["Elements"][key][1], obj["Elements"][key][2], obj["Elements"][key][3], obj["Elements"][key][4]) then
 			
-			ecs.error("Кол-во объектов: "..#obj["Elements"]..", кликнули на объект номер = "..tostring(key)..", #Data = "..#data..", sData ="..sData)
+			--ecs.error("Кол-во объектов: "..#obj["Elements"]..", кликнули на объект номер = "..tostring(key)..", #Data = "..#data..", sData ="..sData)
 
 			drawElement(key, ecs.colors.blue, 0xffffff, e[4])
 			os.sleep(0.3)
