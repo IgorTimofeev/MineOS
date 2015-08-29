73781e7c350689d51dcefbde1059adc3426e24a9
Igor Timofeev <eliteclubsessions@gmail.com>
2015-08-29 22:12:48 +0300

    Update autorun.lua

--- Applications/autorun.lua
+++ Applications/autorun.lua
@@ -1,13 +1,1 @@
 
---Загружаем самые нужные апишки
-_G.component = require("component")
-_G.ecs = require("ECSAPI")
-_G.config = require("config")
-_G.shell = require("shell")
-_G.fs = require("filesystem")
-_G.event = require("event")
-
---Очищаем экран и запускаем ОС
-ecs.clearScreen()
-ecs.setScale(1)
-shell.execute("OS.lua")
