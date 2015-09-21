697b052741c7cf2920c67236efe0364426261de7
Igor Timofeev <eliteclubsessions@gmail.com>
2015-09-21 07:16:39 +0300

    Update Highlight.lua

--- Applications/Highlight/Highlight.lua
+++ Applications/Highlight/Highlight.lua
@@ -1,6 +1,6 @@
 local syntax = require("syntax")
 ecs.prepareToExit()
-local data = ecs.universalWindow("auto", "auto", 40, 0xeeeeee, true, {"EmptyLine"}, {"CenterText", 0x262626, "Enter path to file"}, {"EmptyLine"}, {"Input", 0x262626, 0x880000, "Highlight.app/Resources/TestFile.txt"}, {"EmptyLine"}, {"Button", 0x33db80, 0xffffff, "GO!"})
+local data = ecs.universalWindow("auto", "auto", 40, 0xeeeeee, true, {"EmptyLine"}, {"CenterText", 0x262626, "Enter path to file"}, {"EmptyLine"}, {"Input", 0x262626, 0x880000, "Highlight.app/Resources/TestFile.txt"}, {"EmptyLine"}, {"Button", {0x33db80, 0xffffff, "GO!"}})
 local path = data[1]
 syntax.highlightFileForDebug(path, midnight)
 ecs.waitForTouchOrClick()
