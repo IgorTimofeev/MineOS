d01befad27be92f1820519d909a2a0b92ca7b1bf
Igor Timofeev <eliteclubsessions@gmail.com>
2015-09-27 21:45:04 +0300

    Update Control.lua

--- Applications/Control/Control.lua
+++ Applications/Control/Control.lua
@@ -220,7 +220,7 @@ end
 
 -------------------------------------------------------------------------------------------------------------------------------
 
-ecs.prepareToExit()
+--ecs.prepareToExit()
 
 local oldPixels = ecs.rememberOldPixels(x, y, x + width - 1, y + height - 1)
 drawTopBar()
