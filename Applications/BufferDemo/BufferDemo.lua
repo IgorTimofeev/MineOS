dd8f365a7e6cfdf94f022c2ce618f536b95b606d
Igor Timofeev <Igor@Igor.local>
2015-10-28 10:51:50 +0300

    af

--- Applications/BufferDemo/BufferDemo.lua
+++ Applications/BufferDemo/BufferDemo.lua
@@ -9,7 +9,7 @@ local showPanel = true
 local transparency = 25
 local xWindow, yWindow = 5, 5
 
-local fon = image.load("BufferDemo.app/Wallpaper.pic")
+local fon = image.load("BufferDemo.app/Resources/Wallpaper.pic")
 
 buffer.start()
 
