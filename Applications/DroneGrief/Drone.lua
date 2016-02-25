78b77f155766720c22709b76cab6291bd215877c
Igor <eliteclubsessions@gmail.com>
2016-02-25 12:21:22 +0300

    afaefaefa

--- Applications/DroneGrief/Drone.lua
+++ Applications/DroneGrief/Drone.lua
@@ -5,29 +5,67 @@ local inventory = component.proxy(component.list("inventory_controller")())
 local port = 512
 local moveSpeed = 1.0
 local suckSide = 0
+local direction = 0
+local acceleration = 0.5
 
 modem.open(port)
 
 -------------------------------------
+
+local function move(forward)
+  if direction == 0 then
+      drone.move(1 * forward, 0, 0)
+  elseif direction == 1 then
+    drone.move(0, 0, 1 * forward)
+  elseif direction == 2 then
+    drone.move(-1 * forward, 0, 0)
+  else
+    drone.move(0, 0, -1 * forward)
+  end
+end
+
+local function printSpeed()
+  drone.setStatusText("SPD " .. tostring(moveSpeed))
+end
+
+local function printDirection()
+  drone.setStatusText("DIR " .. tostring(direction))
+end
+
+local function printAcceleration()
+  drone.setStatusText("ACC " .. tostring(acceleration))
+end
+
+local function sendInfo()
+  modem.broadcast(port, "ECSDrone", "DroneInfo", moveSpeed, acceleration, direction)
+end
+
+drone.setStatusText("STARTED")
 
 while true do
   local e = { computer.pullSignal() }
   if e[1] == "modem_message" then
     if e[4] == port then
       if e[6] == "ECSDrone" then
-        drone.setStatusText("Команда: " .. e[7])
+        drone.setStatusText(e[7])
         if e[7] == "moveUp" then
           drone.move(0, moveSpeed, 0)
         elseif e[7] == "moveDown" then
           drone.move(0, -moveSpeed, 0)
         elseif e[7] == "moveForward" then
-          drone.move(moveSpeed, 0, 0)
+          move(1)
         elseif e[7] == "moveBack" then
-          drone.move(-moveSpeed, 0, 0)
-        elseif e[7] == "moveLeft" then
-          drone.move(0, 0, -moveSpeed)
-        elseif e[7] == "moveRight" then
-          drone.move(0, 0, moveSpeed)
+          move(-1)
+        elseif e[7] == "turnLeft" then
+          direction = direction - 1
+          if direction < 0 then direction = 3 end
+          printDirection()
+          sendInfo()
+        elseif e[7] == "turnRight" then
+          direction = direction + 1
+          if direction > 3 then direction = 0 end
+          printDirection()
+          sendInfo()
         elseif e[7] == "changeColor" then
           drone.setLightColor(math.random(0x0, 0xFFFFFF))
         elseif e[7] == "OTSOS" then
@@ -37,16 +75,30 @@ while true do
           for i = 1, (inventory.getInventorySize(1) or 1) do
             inventory.suckFromSlot(1, i)
           end
-        elseif e[7] == "VIBROSI" then
-          for i = 1, drone.inventorySize() do
-            drone.select(i)
-            drone.drop(64)
-          end
+        elseif e[7] == "swing" then
+          drone.swing()
         elseif e[7] == "moveSpeedUp" then
           moveSpeed = moveSpeed + 0.1
+          if moveSpeed >= 3 then moveSpeed = 3 end
+          printSpeed()
+          sendInfo()
         elseif e[7] == "moveSpeedDown" then
           moveSpeed = moveSpeed - 0.1
-          if moveSpeed < 0.1 then moveSpeed = 0.1 end
+          if moveSpeed <= 0.1 then moveSpeed = 0.1 end
+          printSpeed()
+          sendInfo()
+        elseif e[7] == "accelerationUp" then
+          acceleration = acceleration + 0.1
+          if acceleration >= 5 then acceleration = 5 end
+          drone.setAcceleration(acceleration)
+          printAcceleration()
+          sendInfo()
+        elseif e[7] == "accelerationDown" then
+          acceleration = acceleration - 0.1
+          if acceleration <= 0.1 then acceleration = 0.1 end
+          drone.setAcceleration(acceleration)
+          printAcceleration()
+          sendInfo()
         end
       end
     end
--- Applications/DroneGrief/Planshet.lua
+++ Applications/DroneGrief/Planshet.lua
@@ -2,20 +2,29 @@
 local component = require("component")
 local modem = component.modem
 local event = require("event")
+local keyboard = require("keyboard")
 local port = 512
 modem.open(port)
 
 local keys = {
   [17] = "moveForward",
   [31] = "moveBack",
-  [30] = "moveLeft",
-  [32] = "moveRight",
+  [30] = "turnLeft",
+  [32] = "turnRight",
   [42] = "moveDown",
   [57] = "moveUp",
   [46] = "changeColor",
   [18] = "OTSOS",
-  [16] = "VIBROSI",
+  [16] = "swing",
 }
+
+---------------------------------------------------------------------------------------------------------
+
+print(" ")
+print("Добро пожаловать в программу DroneGrief. Используйте клавиши W и S для перемещения дрона, а A и D для смены направления движения. По нажатию SHIFT дрон опустится ниже, а по SPACE - выше. Кнопка E заставит дрона высосать предметы из инвентаря под и над ним, а кнопка C сменит цвет его свечения. При скроллинге колесиком мыши изменяется скорость движения робота, а скроллинг с зажатым ALT изменяет его ускорение.")
+print(" ")
+
+---------------------------------------------------------------------------------------------------------
 
 while true do
   local e = {event.pull()}
@@ -26,11 +35,29 @@ while true do
     end
   elseif e[1] == "scroll" then
     if e[5] == 1 then
-      modem.broadcast(port, "ECSDrone", "moveSpeedUp")
-      print("Увеличить скорость перемещения дрона на 0.1")
+      if keyboard.isAltDown() then
+        modem.broadcast(port, "ECSDrone", "accelerationUp")
+        print("Команда дрону: accelerationUp")
+      else
+        modem.broadcast(port, "ECSDrone", "moveSpeedUp")
+        print("Команда дрону: moveSpeedUp")
+      end
     else
-      print("Уменьшить скорость перемещения дрона на 0.1")
-      modem.broadcast(port, "ECSDrone", "moveSpeedDown")
+      if keyboard.isAltDown() then
+        modem.broadcast(port, "ECSDrone", "accelerationDown")
+        print("Команда дрону: accelerationDown")
+      else
+        modem.broadcast(port, "ECSDrone", "moveSpeedDown")
+        print("Команда дрону: moveSpeedDown")
+      end
+    end
+  elseif e[1] == "modem_message" then
+    if e[6] == "ECSDrone" and e[7] == "DroneInfo" then
+      print(" ")
+      print("Скорость дрона: " .. tostring(e[8]))
+      print("Ускорение дрона: " .. tostring(e[9]))
+      print("Направление дрона: " .. tostring(e[10]))
+      print(" ")
     end
   end
 end
