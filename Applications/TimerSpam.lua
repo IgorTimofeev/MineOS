01dcddef8d11f14db5bb858e04303eb09f0d7091
Igor <eliteclubsessions@gmail.com>
2016-03-06 20:27:40 +0300

    afaef

--- Applications/TimerSpam.lua
+++ Applications/TimerSpam.lua
@@ -4,8 +4,9 @@ local component = require("component")
 local commandBlock = component.command_block
 local chatBox = component.chat_box
 
-local nickname = "Pirnogion"
+local nickname = "PaladinCVM"
 local commanderNickname = "ECS"
+local spamDelay = 1
 
 local function execute(command)
   commandBlock.setCommand(command)
@@ -30,7 +31,7 @@ local function chatDro4er(...)
 			chatBox.say("Спам-бот активирован на цель \"" .. nickname .. "\". Версия 2.4a")
 			_G.spamDro4erID = event.timer(1, spamDro4er, math.huge)
 		elseif e[4] == "деактивировать спам-бота" and _G.spamDro4erID then
-			chatBox.say("Спам-бот деактивирован.")
+			chatBox.say("Спам-бот на цель \"" .. nickname .. "\" деактивирован.")
 			event.cancel(_G.spamDro4erID)
 			_G.spamDro4erID = nil
 		end
