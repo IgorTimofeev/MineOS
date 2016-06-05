f61c2c66ff7bbc83c400c7a82a783a73c8695b24
Igor Timofeev <Igor@iMac-Igor.local>
2016-06-05 13:28:39 +0300

    Приводим в порядок всякое

--- Applications/AppStore/AppStore.lua
+++
@@ -1,2 +0,0 @@
-local ecs = require("ECSAPI")
-ecs.universalWindow("auto", "auto", 30, 0xeeeeee, true, {"EmptyLine"}, {"CenterText", 0x262626, "Hello world!"}, {"EmptyLine"}, {"Button", {0x880000, 0xffffff, "Hello!"}})
