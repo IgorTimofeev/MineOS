936c6975b1d7dc981f3688fac958af3148d6e8be
Igor Timofeev <Igor@iMac-Igor.local>
2017-01-15 03:52:46 +0300

    фуа

--- lib/syntax.lua
+++ lib/syntax.lua
@@ -25,7 +25,7 @@ syntax.colorScheme = {
 	scrollBarBackground = 0x444444,
 	scrollBarForeground = 0x33B6FF,
 	selection = 0x555555,
-	indentation = 0x3C3C3C,
+	indentation = 0x2D2D2D,
 }
 
 syntax.patterns = {
