6c31aa2d7b3dee2c5ba63c9349252680fbacdc3e
Igor Timofeev <igor@iMac-Igor.local>
2017-01-23 03:42:07 +0300

    afaef

---
+++ MineOS/MineOSPacker.lua
@@ -0,0 +1,74 @@
+
+local args = {...}
+local fs = require("filesystem")
+local component = require("component")
+
+------------------------------------------------------------------------------------------------------------
+
+local compressorPath = "/lib/compressor.lua"
+local MineOSPackagePath = "/MineOS.pkg"
+
+local compressorURL = "https://raw.githubusercontent.com/IgorTimofeev/OpenComputers/master/lib/compressor.lua"
+local MineOSPackageURL = "https://raw.githubusercontent.com/IgorTimofeev/OpenComputers/master/MineOS/MineOS.pkg"
+
+local packageFileList = {
+	"/MineOS/",
+	"/lib/",
+	"/OS.lua",
+	"/init.lua",
+	"/autorun.lua",
+}
+
+------------------------------------------------------------------------------------------------------------
+
+function getFile(url, path)
+	local file = io.open(path, "w")
+
+	local pcallSuccess, requestHandle, requestReason = pcall(component.internet.request, url)
+	if pcallSuccess then
+		if requestHandle then
+			while true do
+				local data, reason = requestHandle.read(math.huge)	
+				if data then
+					file:write(data)
+				else
+					requestHandle:close()
+					if not reason then
+						file:close()
+						return
+					end
+				end
+			end
+		end		
+	end
+
+	file:close()
+	print("Failed")
+	os.exit()
+end
+
+local function getCompressor()
+	print("Downloading compressor library...")
+	getFile(compressorURL, compressorPath)
+end
+
+local function getPackage()
+	print("Downloading MineOS package...")
+	getFile(MineOSPackageURL, MineOSPackagePath)
+end
+
+------------------------------------------------------------------------------------------------------------
+
+getCompressor()
+local compressor = dofile(compressorPath)
+
+if args[1] == "pack" then
+	packageFileList[#packageFileList + 1] = true
+	compressor.pack(MineOSPackagePath, table.unpack(packageFileList))
+elseif args[1] == "unpack" then
+	getPackage()
+	compressor.unpack(MineOSPackagePath, "/", true)
+else
+	print("Usage:")
+	print("  MineOSPacker <pack/unpack>")
+end
