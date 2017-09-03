b7a031446a885f76f4c575e6e7afed083ae23d46
Igor Timofeev <igor@iMac-Igor.local>
2017-09-03 19:18:18 +0300

    Обойки

--- Installer/Installer.lua
+++ Installer/Installer.lua
@@ -292,7 +292,7 @@ stages[5] = function()
 	
 	stageContainer:addChild(GUI.label(1, 22, stageContainer.width, 1, 0x666666, localization.needToReboot)):setAlignment(GUI.alignment.horizontal.center, GUI.alignment.vertical.top)
 	stageContainer:addChild(GUI.adaptiveRoundedButton(math.floor(stageContainer.width / 2 - (unicode.len(localization.reboot) + 4) / 2), stageContainer.height - 4, 2, 1, 0xAAAAAA, 0xDDDDDD, 0x777777, 0xDDDDDD, localization.reboot)).onTouch = function()
-		OSSettings.wallpaper = stageContainer.downloadWallpapersSwitch.state and "/MineOS/Pictures/Space.pic" or nil
+		OSSettings.wallpaper = stageContainer.downloadWallpapersSwitch.state and "/MineOS/Pictures/Road.pic" or nil
 		OSSettings.wallpaperEnabled = true
 		OSSettings.transparencyEnabled = true
 		OSSettings.showApplicationIcons = true
