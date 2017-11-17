e07309fa7580dca817016b43c44e20d0fc04ba4b
igor <eliteclubsessions@gmail.com>
2017-11-17 05:42:50 +0300

    Чуть попизже шоб выглядело

--- OS.lua
+++ OS.lua
@@ -545,13 +545,15 @@ local function createOSWindow()
 		local container = MineOSInterface.addUniversalContainer(MineOSInterface.mainContainer, MineOSCore.localization.network)
 		local insertModemTextBox = container.layout:addChild(GUI.textBox(1, 1, 36, 1, nil, 0x555555, {MineOSCore.localization.networkModemNotAvailable}, 1, 0, 0, true, true))
 		
-		local networkNameInput = container.layout:addChild(GUI.input(1, 1, 36, 3, 0xE1E1E1, 0x666666, 0x666666, 0xE1E1E1, 0x2D2D2D, MineOSCore.properties.network.name or "", MineOSCore.localization.networkName))		
 		local stateSwitchAndLabel = container.layout:addChild(GUI.switchAndLabel(1, 1, 36, 8, 0x66DB80, 0x2D2D2D, 0xE1E1E1, 0x888888, MineOSCore.localization.networkState .. ":", MineOSCore.properties.network.enabled))
 		local signalStrengthSlider = container.layout:addChild(GUI.slider(1, 1, 36, 0x66DB80, 0x2D2D2D, 0xE1E1E1, 0x888888, 0, 512, MineOSCore.properties.network.signalStrength, false, MineOSCore.localization.networkSearchRadius ..": ", ""))
 		signalStrengthSlider.roundValues = true
 		signalStrengthSlider.height = 2
 
 		container.layout:addChild(GUI.object(1, 1, 1, 1))
+
+		container.layout:addChild(GUI.label(1, 1, container.width, 1, 0xE1E1E1, MineOSCore.localization.networkName):setAlignment(GUI.alignment.horizontal.center, GUI.alignment.vertical.top))
+		local networkNameInput = container.layout:addChild(GUI.input(1, 1, 36, 3, 0xE1E1E1, 0x666666, 0x666666, 0xE1E1E1, 0x2D2D2D, MineOSCore.properties.network.name or ""))
 
 		container.layout:addChild(GUI.label(1, 1, container.width, 1, 0xE1E1E1, MineOSCore.localization.networkComputers):setAlignment(GUI.alignment.horizontal.center, GUI.alignment.vertical.top))
 		local comboBox = container.layout:addChild(GUI.comboBox(1, 1, 36, 3, 0xE1E1E1, 0x2D2D2D, 0x444444, 0x999999))
@@ -604,7 +606,7 @@ local function createOSWindow()
 			if #networkNameInput.text > 0 then
 				MineOSCore.properties.network.name = networkNameInput.text
 				MineOSCore.saveProperties()
-				
+
 				MineOSNetwork.broadcastComputerState(MineOSCore.properties.network.enabled)
 			end
 		end
