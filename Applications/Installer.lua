2a36fa1e788501f1eb2ff34daf7624655a11223e
igor <eliteclubsessions@gmail.com>
2017-12-10 19:20:13 +0300

    Мяу

--- Applications/Installer.lua
+++ Applications/Installer.lua
@@ -1,143 +1,117 @@
-
-------------------------------------------- Библиотеки -------------------------------------------
-
-local shell = require("shell")
 local component = require("component")
 local unicode = require("unicode")
+local fs = require("filesystem")
 local gpu = component.gpu
+local internet = component.internet
 
-------------------------------------------- Переменные -------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------------
 
---Массив с программами, которые необходимо загрузить. Первый элемент - ссылка на файл, второй - путь для сохранения файла.
+-- Таблица с информацией о файлах, которые необходимо загрузить. Первый элемент - ссылка на файл, второй - путь для сохранения файла.
 local applications = {
-	{ "https://raw.githubusercontent.com/IgorTimofeev/OpenComputers/master/lib/colorlib.lua", "lib/colorlib.lua" },
-	{ "https://raw.githubusercontent.com/IgorTimofeev/OpenComputers/master/lib/ECSAPI.lua", "lib/ECSAPI.lua" },
-	{ "https://raw.githubusercontent.com/IgorTimofeev/OpenComputers/master/lib/context.lua", "lib/context.lua" },
-	{ "https://raw.githubusercontent.com/IgorTimofeev/OpenComputers/master/lib/palette.lua", "lib/palette.lua" },
-	{ "https://raw.githubusercontent.com/IgorTimofeev/OpenComputers/master/lib/image.lua", "lib/image.lua" },
-	{ "https://raw.githubusercontent.com/IgorTimofeev/OpenComputers/master/Applications/Photoshop/Photoshop.lua", "Photoshop.lua" },
+  {
+    url = "https://raw.githubusercontent.com/IgorTimofeev/OpenComputers/master/lib/advancedLua.lua",
+    path = "/lib/advancedLua.lua"
+  },
+  {
+    url = "https://raw.githubusercontent.com/IgorTimofeev/OpenComputers/master/lib/color.lua",
+    path = "/lib/color.lua"
+  },
+  {
+    url = "https://raw.githubusercontent.com/IgorTimofeev/OpenComputers/master/lib/FormatModules/OCIF.lua",
+    path = "/lib/FormatModules/OCIF.lua"
+  },
+  {
+    url = "https://raw.githubusercontent.com/IgorTimofeev/OpenComputers/master/lib/image.lua",
+    path = "/lib/image.lua"
+  },
+  {
+    url = "https://raw.githubusercontent.com/IgorTimofeev/OpenComputers/master/lib/doubleBuffering.lua",
+    path = "/lib/doubleBuffering.lua"
+  }
 }
 
---Получаем размеры монитора
-local xSize, ySize = gpu.getResolution()
+---------------------------------------------------------------------------------------------------------------------------------
 
-------------------------------------------- Функции -------------------------------------------
+local resolutionWidth, resolutionHeight = gpu.getResolution()
 
---Отрисовка квадрата указанного цвета
-local function square(x, y, width, height, background)
-	gpu.setBackground(background)
-	gpu.fill(x, y, width, height, " ")
+function getFile(url, path)
+  local file, fileReason = io.open(path, "w")
+  if file then
+    local pcallSuccess, requestHandle = pcall(internet.request, url)
+    if pcallSuccess then
+      if requestHandle then
+        while true do
+          local data, reason = requestHandle.read(math.huge)  
+          if data then
+            file:write(data)
+          else
+            requestHandle:close()
+            if reason then
+              error(reason)
+            else
+              file:close()
+              return
+            end
+          end
+        end
+      else
+        error("Invalid URL-address: " .. tostring(url))
+      end 
+    else
+      error("Usage: component.internet.request(string url)")
+    end
+
+    file:close()
+  else
+    error("Failed to open file for writing: " .. tostring(fileReason))
+  end
 end
 
---Запомнить область пикселей и возвратить ее в виде массива
-local function rememberOldPixels(x, y, x2, y2)
-	local newPNGMassiv = { ["backgrounds"] = {} }
-	newPNGMassiv.x, newPNGMassiv.y = x, y
-	--Перебираем весь массив стандартного PNG-вида по высоте
-	local xCounter, yCounter = 1, 1
-	for j = y, y2 do
-		xCounter = 1
-		for i = x, x2 do
-			if (i > xSize or i < 0) or (j > ySize or j < 0) then
-				error("Can't remember pixel, because it's located behind the screen: x("..i.."), y("..j..") out of xSize("..xSize.."), ySize("..ySize..")\n")
-			end
-			local symbol, fore, back = gpu.get(i, j)
-			newPNGMassiv["backgrounds"][back] = newPNGMassiv["backgrounds"][back] or {}
-			newPNGMassiv["backgrounds"][back][fore] = newPNGMassiv["backgrounds"][back][fore] or {}
-			table.insert(newPNGMassiv["backgrounds"][back][fore], {xCounter, yCounter, symbol} )
-
-			xCounter = xCounter + 1
-			back, fore, symbol = nil, nil, nil
-		end
-		yCounter = yCounter + 1
-	end
-	return newPNGMassiv
+local function progressBar(x, y, width, height, passiveColor, activeColor, percent)
+  gpu.setForeground(passiveColor)
+  gpu.set(x, y, string.rep("━", width))
+  gpu.setForeground(activeColor)
+  gpu.set(x, y, string.rep("━", math.ceil(width * percent)))
 end
 
---Нарисовать запомненные ранее пиксели из массива
-local function drawOldPixels(massivSudaPihay)
-	--Перебираем массив с фонами
-	for back, backValue in pairs(massivSudaPihay["backgrounds"]) do
-		gpu.setBackground(back)
-		for fore, foreValue in pairs(massivSudaPihay["backgrounds"][back]) do
-			gpu.setForeground(fore)
-			for pixel = 1, #massivSudaPihay["backgrounds"][back][fore] do
-				if massivSudaPihay["backgrounds"][back][fore][pixel][3] ~= transparentSymbol then
-					gpu.set(massivSudaPihay.x + massivSudaPihay["backgrounds"][back][fore][pixel][1] - 1, massivSudaPihay.y + massivSudaPihay["backgrounds"][back][fore][pixel][2] - 1, massivSudaPihay["backgrounds"][back][fore][pixel][3])
-				end
-			end
-		end
-	end
+local function shadowPixel(x, y, symbol)
+  local _, _, background = gpu.get(x, y)
+  gpu.setBackground(background)
+  gpu.set(x, y, symbol)
 end
 
---Ограничение длины строки
-local function stringLimit(mode, text, size, noDots)
-	if unicode.len(text) <= size then return text end
-	local length = unicode.len(text)
-	if mode == "start" then
-		if noDots then
-			return unicode.sub(text, length - size + 1, -1)
-		else
-			return "…" .. unicode.sub(text, length - size + 2, -1)
-		end
-	else
-		if noDots then
-			return unicode.sub(text, 1, size)
-		else
-			return unicode.sub(text, 1, size - 1) .. "…"
-		end
-	end
-end
-
---Шкала прогресса
-local function progressBar(x, y, width, height, background, foreground, percent)
-	local activeWidth = math.ceil(width * percent / 100)
-	square(x, y, width, height, background)
-	square(x, y, activeWidth, height, foreground)
-end
-
---Окно загрузки
 local function downloadWindow()
-	--Задаем стартовые координаты
-	local width, height = 60, 7
-	local progressBarWidth = math.floor(width * 3 / 4)
-	local x, y = math.floor(xSize / 2 - width / 2), math.floor(ySize / 2 - height / 2) + 1
-	--Запоминаем старые пиксели
-	local oldPixels = rememberOldPixels(x, y, x + width - 1, y + height - 1)
-	--Рисуем верхний тулбар
-	square(x, y, width, 1, 0xcccccc)
-	local text = "Загрузка"
-	local xPos, yPos = math.floor(xSize / 2 - unicode.len(text) / 2), y
-	gpu.setForeground( 0x000000 )
-	gpu.set(xPos, yPos, text)
-	--Рисуем само окно
-	square(x, y + 1, width, height - 1, 0xeeeeee)
-	--Загружаем файлы и рисуем шкалу прогресса
-	xPos, yPos = math.floor(xSize / 2 - progressBarWidth / 2), math.floor(ySize / 2)
-	local percent = 0
-	progressBar(xPos, yPos, progressBarWidth, 1, 0xcccccc, 0x3366CC, percent)
-	for i = 1, #applications do
-		progressBar(xPos, yPos, progressBarWidth, 1, 0xcccccc, 0x3366CC, percent)
-		square(xPos, yPos + 1, progressBarWidth, 1, 0xeeeeee)
-		gpu.setForeground(0x262626)
-		gpu.set(xPos, yPos + 1, stringLimit("end", "Устанавливаю " .. applications[i][2], progressBarWidth))
-		shell.execute("wget " .. applications[i][1] .. " " .. applications[i][2] .. " -fQ")
-		percent = math.floor(100 * i / #applications)
-	end
-	progressBar(xPos, yPos, progressBarWidth, 1, 0xcccccc, 0x3366CC, percent)
-	os.sleep(0.3)
-	--Выпиливаем нарисованное окно
-	drawOldPixels(oldPixels)
+  local windowWidth, windowHeight = math.ceil(resolutionWidth * 0.35), 5
+  local progressBarWidth = windowWidth - 4
+  local x, y = math.floor(resolutionWidth / 2 - windowWidth / 2), math.floor(resolutionHeight / 2 - windowHeight / 2)
+
+  gpu.setBackground(0x444444)
+  gpu.fill(x + windowWidth, y + 1, 1, windowHeight - 2, " ")
+  gpu.setForeground(0x444444)
+  for i = x + 1, x + windowWidth do
+    shadowPixel(i, y + windowHeight - 1, "▀")
+  end
+  shadowPixel(x + windowWidth, y, "▄")
+  
+  gpu.setBackground(0xEEEEEE)
+  gpu.fill(x, y, windowWidth, windowHeight - 1, " ")
+
+  local percent = 0
+  for i = 1, #applications do
+    progressBar(x + 2, y + 1, progressBarWidth, 1, 0xCCCCCC, 0x3366CC, i / #applications)
+    gpu.setForeground(0x888888)
+    gpu.set(x + 2, y + 2, string.rep(" ", progressBarWidth))
+    gpu.set(x + 2, y + 2, unicode.sub("Downloading " .. applications[i].path, 1, progressBarWidth))
+    fs.makeDirectory(fs.path(applications[i].path))
+    getFile(applications[i].url, applications[i].path)
+  end
+
+  os.sleep(0.3)
+  
+  gpu.setBackground(0x0)
+  gpu.fill(x, y, windowWidth + 1, windowHeight + 1, " ")
 end
 
-------------------------------------------- Программа -------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------------
 
 downloadWindow()
-
-
-
-
-
-
-
-
