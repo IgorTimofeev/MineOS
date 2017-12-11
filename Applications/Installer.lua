57cd8621547c13161c053f59ccde2ba29b6a4d68
igor <eliteclubsessions@gmail.com>
2017-12-11 18:06:37 +0300

    Meow

--- Applications/Installer.lua
+++ Applications/Installer.lua
@@ -1,117 +1,264 @@
 local component = require("component")
 local unicode = require("unicode")
 local fs = require("filesystem")
+local event = require("event")
 local gpu = component.gpu
 local internet = component.internet
 
 ---------------------------------------------------------------------------------------------------------------------------------
 
--- Таблица с информацией о файлах, которые необходимо загрузить. Первый элемент - ссылка на файл, второй - путь для сохранения файла.
-local applications = {
-  {
-    url = "https://raw.githubusercontent.com/IgorTimofeev/OpenComputers/master/lib/advancedLua.lua",
-    path = "/lib/advancedLua.lua"
-  },
-  {
-    url = "https://raw.githubusercontent.com/IgorTimofeev/OpenComputers/master/lib/color.lua",
-    path = "/lib/color.lua"
-  },
-  {
-    url = "https://raw.githubusercontent.com/IgorTimofeev/OpenComputers/master/lib/FormatModules/OCIF.lua",
-    path = "/lib/FormatModules/OCIF.lua"
-  },
-  {
-    url = "https://raw.githubusercontent.com/IgorTimofeev/OpenComputers/master/lib/image.lua",
-    path = "/lib/image.lua"
-  },
-  {
-    url = "https://raw.githubusercontent.com/IgorTimofeev/OpenComputers/master/lib/doubleBuffering.lua",
-    path = "/lib/doubleBuffering.lua"
-  }
+-- Specify required files for downloading
+local files = {
+	{
+		url = "https://raw.githubusercontent.com/IgorTimofeev/OpenComputers/master/lib/advancedLua.lua",
+		path = "/lib/advancedLua.lua"
+	},
+	{
+		url = "https://raw.githubusercontent.com/IgorTimofeev/OpenComputers/master/lib/color.lua",
+		path = "/lib/color.lua"
+	},
+	{
+		url = "https://raw.githubusercontent.com/IgorTimofeev/OpenComputers/master/lib/FormatModules/OCIF.lua",
+		path = "/lib/FormatModules/OCIF.lua"
+	},
+	{
+		url = "https://raw.githubusercontent.com/IgorTimofeev/OpenComputers/master/lib/image.lua",
+		path = "/lib/image.lua"
+	},
+	{
+		url = "https://raw.githubusercontent.com/IgorTimofeev/OpenComputers/master/lib/doubleBuffering.lua",
+		path = "/lib/doubleBuffering.lua"
+	},
+	{
+		url = "https://raw.githubusercontent.com/IgorTimofeev/OpenComputers/master/lib/syntax.lua",
+		path = "/lib/syntax.lua"
+	},
+	{
+		url = "https://raw.githubusercontent.com/IgorTimofeev/OpenComputers/master/lib/palette.lua",
+		path = "/lib/palette.lua"
+	},
+	{
+		url = "https://raw.githubusercontent.com/IgorTimofeev/OpenComputers/master/lib/GUI.lua",
+		path = "/lib/GUI.lua"
+	},
+}
+
+local properties = {
+	-- Comment any coordinate to calculate it automatically (will centerize window on screen by specified axis)
+	windowX = 2,
+	windowY = 2,
+	-- Set window width value lower than zero (0.5 for example) to calculate it dependent on screen width
+	windowWidth = 54,
+	-- Customize offset by X axis from window corners
+	GUIElementsOffset = 1,
+	-- Customize localization as you want to
+	localization = {
+		-- Specify title of your installer
+		title = "GUI framework installer",
+		-- Use %s for current file path
+		currentFile = "Downloading \"%s\"",
+		-- Use %s for total (rounded) progress
+		totalProgress = "Total progress: %s%%",
+		-- Comment this lines to automatically close installer window
+		finished1 = "GUI framework has been successfully installed",
+		finished2 = "Press any key to quit"
+	},
+	-- Customize color scheme as you want to
+	colors = {
+		window = {
+			background = 0xEEEEEE,
+			text = 0x999999,
+			shadow = 0x3C3C3C
+		},
+		title = {
+			background = 0xCCCCCC,
+			text = 0x555555,
+		},
+		progressBar = {
+			active = 0x0092FF,
+			passive = 0xCCCCCC
+		}
+	}
 }
 
 ---------------------------------------------------------------------------------------------------------------------------------
 
-local resolutionWidth, resolutionHeight = gpu.getResolution()
+local screenWidth, screenHeight = gpu.getResolution()
+properties.windowHeight = 8
 
-function getFile(url, path)
-  local file, fileReason = io.open(path, "w")
-  if file then
-    local pcallSuccess, requestHandle = pcall(internet.request, url)
-    if pcallSuccess then
-      if requestHandle then
-        while true do
-          local data, reason = requestHandle.read(math.huge)  
-          if data then
-            file:write(data)
-          else
-            requestHandle:close()
-            if reason then
-              error(reason)
-            else
-              file:close()
-              return
-            end
-          end
-        end
-      else
-        error("Invalid URL-address: " .. tostring(url))
-      end 
-    else
-      error("Usage: component.internet.request(string url)")
-    end
+if properties.windowWidth < 1 then
+	properties.windowWidth = math.floor(screenWidth * properties.windowWidth)
+end
+progressBarWidth = properties.windowWidth - properties.GUIElementsOffset * 2
 
-    file:close()
-  else
-    error("Failed to open file for writing: " .. tostring(fileReason))
-  end
+if not properties.windowX then
+	properties.windowX = math.floor(screenWidth / 2 - properties.windowWidth / 2)
 end
 
-local function progressBar(x, y, width, height, passiveColor, activeColor, percent)
-  gpu.setForeground(passiveColor)
-  gpu.set(x, y, string.rep("━", width))
-  gpu.setForeground(activeColor)
-  gpu.set(x, y, string.rep("━", math.ceil(width * percent)))
+if not properties.windowY then
+	properties.windowY = math.floor(screenHeight / 2 - properties.windowHeight / 2)
 end
 
-local function shadowPixel(x, y, symbol)
-  local _, _, background = gpu.get(x, y)
-  gpu.setBackground(background)
-  gpu.set(x, y, symbol)
+local currentBackground, currentForeground
+
+---------------------------------------------------------------------------------------------------------------------------------
+
+local function setBackground(color)
+	if currentBackground ~= color then
+		gpu.setBackground(color)
+		currentBackground = color
+	end
 end
 
-local function downloadWindow()
-  local windowWidth, windowHeight = math.ceil(resolutionWidth * 0.35), 5
-  local progressBarWidth = windowWidth - 4
-  local x, y = math.floor(resolutionWidth / 2 - windowWidth / 2), math.floor(resolutionHeight / 2 - windowHeight / 2)
+local function setForeground(color)
+	if currentForeground ~= color then
+		gpu.setForeground(color)
+		currentForeground = color
+	end
+end
 
-  gpu.setBackground(0x444444)
-  gpu.fill(x + windowWidth, y + 1, 1, windowHeight - 2, " ")
-  gpu.setForeground(0x444444)
-  for i = x + 1, x + windowWidth do
-    shadowPixel(i, y + windowHeight - 1, "▀")
-  end
-  shadowPixel(x + windowWidth, y, "▄")
-  
-  gpu.setBackground(0xEEEEEE)
-  gpu.fill(x, y, windowWidth, windowHeight - 1, " ")
+local function rectangle(x, y, width, height, color)
+	setBackground(color)
+	gpu.fill(x, y, width, height, " ")
+end
 
-  local percent = 0
-  for i = 1, #applications do
-    progressBar(x + 2, y + 1, progressBarWidth, 1, 0xCCCCCC, 0x3366CC, i / #applications)
-    gpu.setForeground(0x888888)
-    gpu.set(x + 2, y + 2, string.rep(" ", progressBarWidth))
-    gpu.set(x + 2, y + 2, unicode.sub("Downloading " .. applications[i].path, 1, progressBarWidth))
-    fs.makeDirectory(fs.path(applications[i].path))
-    getFile(applications[i].url, applications[i].path)
-  end
+local function centerizedText(y, color, text)
+	local textLength = unicode.len(text)
+	if textLength > progressBarWidth then
+		text = unicode.sub(text, 1, progressBarWidth)
+	end
 
-  os.sleep(0.3)
-  
-  gpu.setBackground(0x0)
-  gpu.fill(x, y, windowWidth + 1, windowHeight + 1, " ")
+	setForeground(color)
+	gpu.set(properties.windowX + properties.GUIElementsOffset, y, string.rep(" ", progressBarWidth))
+	gpu.set(math.floor(properties.windowX + properties.GUIElementsOffset + progressBarWidth / 2 - textLength / 2), y, text)
+end
+
+local function progressBar(y, percent, text)
+	setForeground(properties.colors.progressBar.passive)
+	gpu.set(properties.windowX + properties.GUIElementsOffset, y, string.rep("━", progressBarWidth))
+	setForeground(properties.colors.progressBar.active)
+	gpu.set(properties.windowX + properties.GUIElementsOffset, y, string.rep("━", math.ceil(progressBarWidth * percent)))
+
+	centerizedText(y + 1, properties.colors.window.text, text)
+end
+
+local function download(url, path)
+	fs.makeDirectory(fs.path(path))
+
+	local file, fileReason = io.open(path, "w")
+	if file then
+		local pcallSuccess, requestHandle = pcall(internet.request, url)
+		if pcallSuccess then
+			if requestHandle then
+				-- Drawing progressbar once with zero percentage
+				local y = properties.windowY + 2
+				progressBar(y, 0, string.format(properties.localization.currentFile, path))
+				
+				-- Waiting for any response code
+				local responseCode, responseName, responseData
+				repeat
+					responseCode, responseName, responseData = requestHandle:response()
+				until responseCode
+
+				-- Downloading file by chunks
+				if responseData and responseData["Content-Length"] then
+					local contentLength = tonumber(responseData["Content-Length"][1])
+					local currentLength = 0
+					while true do
+						local data, reason = requestHandle.read(math.huge)
+						if data then
+							currentLength = currentLength + unicode.len(data)
+							progressBar(y, currentLength / contentLength, string.format(properties.localization.currentFile, path))
+
+							file:write(data)
+						else
+							requestHandle:close()
+							if reason then
+								error(reason)
+							else
+								file:close()
+								return
+							end
+						end
+					end
+				else
+					error("Response Content-Length header is missing: " .. tostring(responseCode) .. " " .. tostring(responseName))
+				end
+			else
+				error("Invalid URL-address: " .. tostring(url))
+			end 
+		else
+			error("Usage: component.internet.request(string url)")
+		end
+
+		file:close()
+	else
+		error("Failed to open file for writing: " .. tostring(fileReason))
+	end
 end
 
 ---------------------------------------------------------------------------------------------------------------------------------
 
-downloadWindow()
+-- Copying current screen data
+local oldPixels = {}
+for y = properties.windowY, properties.windowY + properties.windowHeight do
+	oldPixels[y] = {}
+	for x = properties.windowX, properties.windowX + properties.windowWidth do
+		oldPixels[y][x] = { gpu.get(x, y) }
+	end
+end
+
+local function shadowPixel(x, y, symbol)
+	setBackground(oldPixels[y][x][3])
+	gpu.set(x, y, symbol)
+end
+
+-- Vertical shadow
+rectangle(properties.windowX + properties.windowWidth, properties.windowY + 1, 1, properties.windowHeight - 1, properties.colors.window.shadow)
+setForeground(properties.colors.window.shadow)
+shadowPixel(properties.windowX + properties.windowWidth, properties.windowY, "▄")
+
+-- Horizontal shadow
+for i = properties.windowX + 1, properties.windowX + properties.windowWidth do
+	shadowPixel(i, properties.windowY + properties.windowHeight, "▀")
+end
+
+-- Window background
+rectangle(properties.windowX, properties.windowY + 1, properties.windowWidth, properties.windowHeight - 1, properties.colors.window.background)
+
+-- Title
+rectangle(properties.windowX, properties.windowY, properties.windowWidth, 1, properties.colors.title.background)
+centerizedText(properties.windowY, properties.colors.title.text, properties.localization.title)
+setBackground(properties.colors.window.background)
+
+-- Downloading
+local y = properties.windowY + 5
+progressBar(y, 0, string.format(properties.localization.totalProgress, "0"))
+for i = 1, #files do
+	local percent = i / #files
+	download(files[i].url, files[i].path)
+	progressBar(y, percent, string.format(properties.localization.totalProgress, tostring(math.ceil(percent * 100))))
+end
+
+-- On exit
+if properties.localization.finished1 then
+	rectangle(properties.windowX, properties.windowY + 1, properties.windowWidth, properties.windowHeight - 1, properties.colors.window.background)
+	centerizedText(properties.windowY + 3, properties.colors.window.text, properties.localization.finished1)
+	centerizedText(properties.windowY + 4, properties.colors.window.text, properties.localization.finished2)
+
+	while true do
+		local eventType = event.pull()
+		if eventType == "key_down" or eventType == "touch" then
+			break
+		end
+	end
+end
+
+-- Redraw old pixels
+for y = properties.windowY, properties.windowY + properties.windowHeight do
+	for x = properties.windowX, properties.windowX + properties.windowWidth do
+		setBackground(oldPixels[y][x][3])
+		setForeground(oldPixels[y][x][2])
+		gpu.set(x, y, oldPixels[y][x][1])
+	end
+end
