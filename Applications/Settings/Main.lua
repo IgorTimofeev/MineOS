ac06087972476d654156a269165f6cb97e779b99
Igor Timofeev <methodman1@mail.ru>
2018-09-17 16:13:12 +0300

    Сделал попизже дизайн приложухи настроек

--- Applications/Settings/Main.lua
+++ Applications/Settings/Main.lua
@@ -21,25 +21,22 @@ local scrollSpeed = 2
 
 local mainContainer, window = MineOSInterface.addWindow(GUI.filledWindow(1, 1, 100, 29, 0xF0F0F0))
 
-local leftPanel = window:addChild(GUI.panel(1, 1, 30, 1, 0xE1E1E1))
-window.backgroundPanel.localX = leftPanel.width + 1
-
+local leftPanel = window:addChild(GUI.panel(1, 1, 1, 1, 0x2D2D2D))
 window.actionButtons.localY = 2
 window.actionButtons:moveToFront()
 
-local modulesLayout = window:addChild(GUI.layout(1, 3, leftPanel.width, 1, 1, 1))
+local modulesLayout = window:addChild(GUI.layout(1, 3, 1, 1, 1, 1))
 modulesLayout:setAlignment(1, 1, GUI.ALIGNMENT_HORIZONTAL_LEFT, GUI.ALIGNMENT_VERTICAL_TOP)
 modulesLayout:setSpacing(1, 1, 1)
 
-window.contentLayout = window:addChild(GUI.layout(window.backgroundPanel.localX, 1, 1, 1, 1, 1))
-
+window.contentLayout = window:addChild(GUI.layout(1, 1, 1, 1, 1, 1))
 
 local function moduleDraw(object)
-	local textColor = object.pressed and 0xE1E1E1 or 0x2D2D2D
+	local textColor = object.pressed and 0x3C3C3C or 0xE1E1E1
 	if object.pressed then
-		buffer.drawRectangle(object.x, object.y, object.width, object.height, 0x336DFF, textColor, " ")
-		buffer.drawText(object.x, object.y - 1, 0x336DFF, string.rep("▄", object.width))
-		buffer.drawText(object.x, object.y + object.height, 0x336DFF, string.rep("▀", object.width))
+		buffer.drawRectangle(object.x, object.y, object.width, object.height, 0xF0F0F0, textColor, " ")
+		buffer.drawText(object.x, object.y - 1, 0xF0F0F0, string.rep("▄", object.width))
+		buffer.drawText(object.x, object.y + object.height, 0xF0F0F0, string.rep("▀", object.width))
 	end
 
 	buffer.drawImage(object.x + 2, object.y, object.icon)
@@ -86,32 +83,6 @@ local function moduleEventHandler(mainContainer, object, e1)
 	end
 end
 
-local modules = {}
-for file in filesystem.list(modulesPath) do
-	table.insert(modules, file)
-end
-table.sort(modules, function(a, b) return a < b end)
-
-for i = 1, #modules do
-	local result, reason = loadfile(modulesPath .. modules[i] .. "Main.lua")
-	if result then
-		local success, result = pcall(result, mainContainer, window, localization)
-		if success then
-			local object = modulesLayout:addChild(GUI.object(1, 1, modulesLayout.width, 3))
-
-			object.icon = image.load(modulesPath .. modules[i] .. "Icon.pic")
-			object.module = result
-			object.pressed = false
-			object.draw = moduleDraw
-			object.eventHandler = moduleEventHandler
-		else
-			error("Failed to execute module " .. modules[i] .. ": " .. tostring(result))
-		end
-	else
-		error("Failed to load module " .. modules[i] .. ": " .. tostring(reason))
-	end
-end
-
 modulesLayout.eventHandler = function(mainContainer, object, e1, e2, e3, e4, e5)
 	if e1 == "scroll" then
 		local cell = modulesLayout.cells[1][1]
@@ -132,7 +103,7 @@ window.onResize = function(width, height)
 	modulesLayout:setMargin(1, 1, 0, 1)
 
 	window.backgroundPanel.width, window.backgroundPanel.height = width - leftPanel.width, height
-	window.contentLayout.width, window.contentLayout.height = window.backgroundPanel.width, window.backgroundPanel.height
+	window.contentLayout.localX, window.contentLayout.width, window.contentLayout.height = window.backgroundPanel.localX, window.backgroundPanel.width, window.backgroundPanel.height
 	leftPanel.height = height
 	modulesLayout.height = height - 2
 
@@ -142,6 +113,42 @@ window.onResize = function(width, height)
 			break
 		end
 	end
+end
+
+--------------------------------------------------------------------------------
+
+local modules = {}
+for file in filesystem.list(modulesPath) do
+	table.insert(modules, file)
+end
+table.sort(modules, function(a, b) return a < b end)
+
+for i = 1, #modules do
+	local result, reason = loadfile(modulesPath .. modules[i] .. "Main.lua")
+	if result then
+		local success, result = pcall(result, mainContainer, window, localization)
+		if success then
+			local object = modulesLayout:addChild(GUI.object(1, 1, 1, 3))
+
+			object.icon = image.load(modulesPath .. modules[i] .. "Icon.pic")
+			object.module = result
+			object.pressed = false
+			object.draw = moduleDraw
+			object.eventHandler = moduleEventHandler
+
+			leftPanel.width = math.max(leftPanel.width, unicode.len(result.name) + 14)
+		else
+			error("Failed to execute module " .. modules[i] .. ": " .. tostring(result))
+		end
+	else
+		error("Failed to load module " .. modules[i] .. ": " .. tostring(reason))
+	end
+end
+
+modulesLayout.width = leftPanel.width
+window.backgroundPanel.localX = leftPanel.width + 1
+for i = 1, #modulesLayout.children do
+	modulesLayout.children[i].width = leftPanel.width
 end
 
 window:resize(window.width, window.height)
