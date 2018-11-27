1b51cd8286cf90979b0c0710c76c1897a8a9b43b
Igor Timofeev <methodman1@mail.ru>
2018-11-27 20:24:56 +0300

    User add feature for Settings app

---
+++ Applications/Settings/Modules/00_Users/Main.lua
@@ -0,0 +1,67 @@
+
+local GUI = require("GUI")
+local computer = require("computer")
+
+local module = {}
+
+local mainContainer, window, localization = table.unpack({...})
+
+--------------------------------------------------------------------------------
+
+module.name = localization.users
+module.margin = 0
+module.onTouch = function()
+	window.contentLayout:addChild(GUI.text(1, 1, 0x2D2D2D, localization.usersAdd))
+
+	local input = window.contentLayout:addChild(GUI.input(1, 1, 36, 3, 0xE1E1E1, 0x696969, 0xA5A5A5, 0xE1E1E1, 0x2D2D2D, "", localization.usersTypeNameHere))
+
+	window.contentLayout:addChild(GUI.textBox(1, 1, 36, 1, nil, 0xA5A5A5, {localization.usersInfo}, 1, 0, 0, true, true))
+
+	local usersListText = window.contentLayout:addChild(GUI.text(1, 1, 0x2D2D2D, localization.usersList))
+
+	local usersLayout = window.contentLayout:addChild(GUI.layout(1, 1, 36, 1, 1, 1))
+	usersLayout:setAlignment(1, 1, GUI.ALIGNMENT_HORIZONTAL_LEFT, GUI.ALIGNMENT_VERTICAL_TOP)
+	usersLayout:setSpacing(1, 1, 0)
+
+	local function update()
+		local users = {computer.users()}
+		
+		usersLayout:removeChildren()
+		usersLayout.height = 0
+
+		usersListText.hidden = #users == 0
+		usersLayout.hidden = usersListText.hidden
+
+		if #users > 0 then
+			for i = 1, #users do
+				local userContainer = usersLayout:addChild(GUI.container(1, 1, usersLayout.width, 3))
+				userContainer:addChild(GUI.panel(1, 1, userContainer.width - 5, userContainer.height, 0xE1E1E1))
+				userContainer:addChild(GUI.text(2, 2, 0x696969, string.limit(users[i], userContainer.width - 5, "right")))
+				userContainer:addChild(GUI.button(userContainer.width - 4, 1, 5, 3, 0xD2D2D2, 0x0, 0x969696, 0xE1E1E1, "x")).onTouch = function()
+					computer.removeUser(users[i])
+
+					update()
+					mainContainer:drawOnScreen()
+				end
+
+				usersLayout.height = usersLayout.height + userContainer.height
+			end
+		end
+	end
+
+	input.onInputFinished = function()
+		if #input.text > 0 then
+			computer.addUser(input.text)
+			input.text = ""
+
+			update()
+			mainContainer:drawOnScreen()
+		end
+	end
+
+	update()
+end
+
+--------------------------------------------------------------------------------
+
+return module
