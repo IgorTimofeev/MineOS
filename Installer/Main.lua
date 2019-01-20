83624178bfce40140bc7417773df91c4757e4c80
Igor Timofeev <igor@iMac-Igor.local>
2019-01-20 22:24:17 +0300

    afaef

--- Installer/Main.lua
+++ Installer/Main.lua
@@ -423,7 +423,7 @@ addStage(function()
 				for i = 1, #list do
 					path = "/" .. list[i]
 
-					if proxy ~= temporaryFilesystemProxy or path ~= installerPath then
+					if proxy.address ~= temporaryFilesystemProxy.address or path ~= installerPath then
 						proxy.remove(path)
 					end
 				end
