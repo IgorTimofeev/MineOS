75fb30ce78b8b17b5efbc378bdc27f5eff540a3a
Igor Timofeev <igor@iMac-Igor.local>
2019-01-24 17:47:17 +0300

    FIXXXX

--- Installer/Main.lua
+++ Installer/Main.lua
@@ -213,7 +213,7 @@ window:addChild(GUI.panel(1, 1, window.width, window.height, 0xE1E1E1))
 
 -- Top menu
 local menu = workspace:addChild(GUI.menu(1, 1, workspace.width, 0xF0F0F0, 0x787878, 0x3366CC, 0xE1E1E1))
-local installerMenu = menu:addContextMenu("MineOS", 0x2D2D2D)
+local installerMenu = menu:addContextMenuItem("MineOS", 0x2D2D2D)
 installerMenu:addItem("Shutdown").onTouch = function()
 	computer.shutdown()
 end
@@ -509,7 +509,7 @@ addStage(function()
 	filesystem.setProxy(selectedFilesystemProxy)
 
 	paths.create(paths.system)
-	local userProperties, userPaths = system.createUser(
+	local userSettings, userPaths = system.createUser(
 		usernameInput.text,
 		localizationComboBox:getItem(localizationComboBox.selectedItem).text,
 		not passwordSwitchAndLabel.switch.state and passwordInput.text,
