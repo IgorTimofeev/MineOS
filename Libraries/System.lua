f1bcf3a7e8f4503323d8544507c7db2bd6576275
MFernflower <machacker9000@gmail.com>
2020-05-24 23:23:47 -0400

    Fix screensaver default time being too short

--- Libraries/System.lua
+++ Libraries/System.lua
@@ -92,7 +92,7 @@ function system.getDefaultUserSettings()
 
 		interfaceScreensaverEnabled = false,
 		interfaceScreensaverPath = paths.system.screensavers .. "Space.lua",
-		interfaceScreensaverDelay = 20,
+		interfaceScreensaverDelay = 50,
 		
 		interfaceTransparencyEnabled = true,
 		interfaceTransparencyDock = 0.4,
