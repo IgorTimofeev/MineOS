dfaf83f2f5a9db6756caa49f6e9d9a8b7b70d570
QuickMuffin8782 <86572544+QM8782@users.noreply.github.com>
2024-01-02 19:35:51 -0600

    Update Main.lua

--- Installer/Main.lua
+++ Installer/Main.lua
@@ -13,7 +13,7 @@ local EEPROMAddress, internetAddress, GPUAddress =
 component.invoke(GPUAddress, "bind", getComponentAddress("screen"))
 local screenWidth, screenHeight = component.invoke(GPUAddress, "getResolution")
 
-local repositoryURL = "https://raw.githubusercontent.com/IgorTimofeev/MineOS/master/"
+local repositoryURL = "https://raw.githubusercontent.com/QM8782/MineOS/master/"
 local installerURL = "Installer/"
 local EFIURL = "EFI/Minified.lua"
 
