de9d3fed8e55db6fde33a86b1bc7e6b303dfee04
IgorTimofeev <methodman1@mail.ru>
2024-01-15 23:21:30 +0300

    СУКА

--- Applications/Settings.app/Modules/2_Icons/Main.lua
+++ Applications/Settings.app/Modules/2_Icons/Main.lua
@@ -24,21 +24,6 @@ module.onTouch = function()
 	
 	window.contentLayout:addChild(GUI.textBox(1, 1, 36, 1, nil, 0xA5A5A5, {localization.appearanceTransparencyInfo}, 1, 0, 0, true, true))
 
-	window.contentLayout:addChild(GUI.text(1, 1, 0x2D2D2D, localization.appearanceColorScheme))
-
-	local function addColorSelector(key, ...)
-		local c = window.contentLayout:addChild(GUI.colorSelector(1, 1, 36, 1, userSettings[key], ...))
-		c.onColorSelected = function()
-			userSettings[key] = c.color
-
-			system.updateColorScheme()
-			workspace:draw()
-			system.saveUserSettings()
-		end
-	end
-
-	addColorSelector("interfaceColorDesktopBackground", localization.appearanceDesktopBackground)
-
 	window.contentLayout:addChild(GUI.text(1, 1, 0x2D2D2D, localization.appearanceSize))
 
 	local iconWidthSlider = window.contentLayout:addChild(GUI.slider(1, 1, 36, 0x66DB80, 0xE1E1E1, 0xFFFFFF, 0xA5A5A5, 8, 16, userSettings.iconWidth, false, localization.appearanceHorizontal .. ": ", ""))
